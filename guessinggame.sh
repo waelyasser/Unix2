cdf8012095fe73f62c47189c89da5e39b4312a05
waelyasser <waelyasser23@gmail.com>
2024-05-08 12:33:10 +0300

    assignment done

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash 
 # File: guessinggame.sh
-
+# project: guessinggnumber
 function guessinggame(){
 	no_of_files=$(pwd | ls | wc -l)     
 	while true;     
 	do
-		echo "pleas enter your guess so that I can help yuou!!"         
-		read  number         
-		if [ $number -lt $no_of_files ]         
+		echo "My dear user , please enter your guess so that I can help you!!"         
+		read  entry         
+		if [ $entry -lt $no_of_files ]         
 		then
 			echo "Your guess is less than the number of files that are in the directory"
-		elif [ $number -gt $no_of_files ]         
+		elif [ $entry -gt $no_of_files ]         
 		then
 			 echo "Your guess is greater than the number of files that are in the directory"
-		elif [ $number -eq $no_of_files ] 
-		then	echo "Congratulation,your guess is correct, and you nailed it"
+		elif [ $entry -eq $no_of_files ] 
+		then	echo "Congratulations my dear user!!!,your guess is correct, and you nailed it"
 		break;
 		fi
 	done
     }
-echo "Guess how many files are there in current directory right now?"
 guessinggame
 
